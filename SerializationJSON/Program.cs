2d2645cc3e7da857fffb1c6f3e3eedf2b01aeabb
JayMackay <jared.mackay1994@live.com>
2020-02-26 13:20:29 +0000

    Serialize Initiated Class Object & Deserialize

---
+++ SerializationJSON/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SerializationJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initilizing New Trainee Object
+            Trainee traineeObj = new Trainee();
+            traineeObj.Name = "Jared Mackay";
+            traineeObj.Age = 25;
+            traineeObj.DateOfBirth = new DateTime(1994, 09, 12);
+
+            //SERIALIZATION
+            //Serialize the Trainee Object as a string
+            string objectOutput = JsonConvert.SerializeObject(traineeObj);
+            Console.WriteLine(objectOutput);
+
+            //DESERIALIZATION
+            //Deserialize the serialized "objectOutput" string
+            Trainee deserializedTrainee = JsonConvert.DeserializeObject<Trainee>(objectOutput);
+            Console.WriteLine(deserializedTrainee.Name);
+            Console.WriteLine(deserializedTrainee.Age);
+            Console.WriteLine(deserializedTrainee.DateOfBirth);
+        }
+    }
+}
---
+++ SerializationJSON/Trainee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializationJSON
+{
+    class Trainee
+    {
+        //Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
